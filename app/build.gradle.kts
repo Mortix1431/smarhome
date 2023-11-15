75772403c2e66c7060865f8c65d00e4f8e301310
Носенко Александр Константинович <pr130nosenko@oat.local>
2023-11-15 16:42:05 +0600

    day 10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,11 +38,15 @@ android {
 
 dependencies {
 
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation(platform("io.github.jan-tennert.supabase:bom:1.4.3"))
     implementation("io.github.jan-tennert.supabase:postgrest-kt")
     implementation("io.github.jan-tennert.supabase:realtime-kt")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2")
+
+    implementation ("androidx.recyclerview:recyclerview:1.2.0")
+    implementation ("androidx.cardview:cardview:1.0.0")
 
     implementation("io.ktor:ktor-client-cio:2.3.4")
     implementation("androidx.appcompat:appcompat:1.6.1")
@@ -51,4 +55,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
